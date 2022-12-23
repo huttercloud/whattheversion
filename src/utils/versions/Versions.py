cd69a02cb9f0bbac4ccb937877ee12158290c6cb
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-23 10:53:00 +0100

    FIx empty default list

--- src/utils/versions/Versions.py
+++ src/utils/versions/Versions.py
@@ -1,4 +1,5 @@
 from pydantic.dataclasses import dataclass
+from dataclasses import field
 from typing import List, Optional
 
 from ..git.Git import GitRepository
@@ -7,7 +8,7 @@ from ..helm import HelmRepository
 
 @dataclass
 class VersionsRequest(object):
-    git: Optional[List[GitRepository]] = list()
-    docker: Optional[List[DockerRepository]] = list()
-    helm: Optional[List[HelmRepository]] = list()
+    git: Optional[List[GitRepository]] = field(default_factory=list)
+    docker: Optional[List[DockerRepository]] = field(default_factory=list)
+    helm: Optional[List[HelmRepository]] = field(default_factory=list)
 
