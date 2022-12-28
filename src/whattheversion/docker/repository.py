0c5d207fd33572b2ced93a5f0394fc2b2abde118
Sebastian Hutter <mail@sebastian-hutter.ch>
2022-12-29 00:09:10 +0100

    Cleanup aiohttp

--- src/whattheversion/docker/repository.py
+++ src/whattheversion/docker/repository.py
@@ -1,10 +1,7 @@
 from typing import Dict, List
 import requests
-import aiohttp
-import asyncio
-from ..models import DockerImageTag, Versions, Version
+from ..models import DockerImageTag
 from datetime import datetime
-import json
 import logging
 
 from ..models import DockerImageTags
@@ -131,122 +128,6 @@ class DockerRepositoryV2(object):
                 logging.warning(ex)
 
         return docker_image_tags_with_timestamp
-
-    # def convert_to_versions(self) -> Versions:
-    #     """
-    #     convert the list of tags to a versions list for client response
-    #
-    #     :return:
-    #     """
-    #
-    #     v = Versions(versions=[])
-    #     for t in self.tags:
-    #         v.versions.append(Version(version=t.tag, timestamp=t.created))
-    #
-    #     return v
-
-    # def _get_tags_with_metadata(self) -> List[DockerImageTag]:
-    #     """
-    #     return all tags with their digest and creation date
-    #     :return:
-    #     """
-    #
-    #     # get all tags (sync)
-    #     # then get all digest sums (async)
-    #     # and then all created timestamps from the corresponding blocks (async)
-    #     tags = self._get_tags()
-    #
-    #     loop = asyncio.get_event_loop()
-    #     tags_with_digest = loop.run_until_complete(self._get_digest_for_tags(tags=tags, loop=loop))
-    #     tags_with_digest_and_timestamp = loop.run_until_complete(self._get_timestamp_for_digests(tags_with_digest=tags_with_digest, loop=loop))
-    #
-    #     docker_image_tags = []
-    #     for t in tags_with_digest_and_timestamp:
-    #         if not t:
-    #             continue
-    #         docker_image_tags.append(DockerImageTag(**t))
-    #
-    #     return docker_image_tags
-    #
-    # async def _get_digest_for_tag(self, session: aiohttp.ClientSession, tag: str) -> Dict[str, str]:
-    #     async with session.get(f'{self.repository_base_url}/manifests/{tag}') as r:
-    #         try:
-    #             response = await r.json()
-    #             r.raise_for_status()
-    #             return dict(tag=tag, digest=response['config']['digest'])
-    #         except Exception as ex:
-    #             logging.warning(ex)
-    #             return dict()
-    #
-    # async def _get_digest_for_tags(self, tags: List[str], loop) -> List[Dict[str, str]]:
-    #     """
-    #     retrieve digest and digest creation date with aiohttp to speed up the process
-    #
-    #     :param tags:
-    #     :return:
-    #     """
-    #
-    #     session_digest = aiohttp.ClientSession(
-    #         loop=loop,
-    #         headers={
-    #             **self.http_headers,
-    #             **{'Accept': 'application/vnd.docker.distribution.manifest.v2+json'}
-    #         },
-    #         connector = aiohttp.TCPConnector(
-    #             limit_per_host=50
-    #         )
-    #     )
-    #
-    #     tags_with_digest = await asyncio.wait([loop.create_task(self._get_digest_for_tag(session=session_digest, tag=t)) for t in tags])
-    #     await session_digest.close()
-    #
-    #     results = []
-    #     for r in tags_with_digest[0]:
-    #         re = r.result()
-    #         results.append(re)
-    #
-    #     return results
-    #
-    # async def _get_timestamp_for_digest(self, session: aiohttp.ClientSession, tag: str, digest: str):
-    #     async with session.get(f'{self.repository_base_url}/blobs/{digest}') as r:
-    #         try:
-    #             # application/octet-stream and not json is received from endpoint, no .json() for this part
-    #             response = await r.text()
-    #             r.raise_for_status()
-    #             created = datetime.strptime(json.loads(response)["created"][0:19], '%Y-%m-%dT%H:%M:%S')
-    #             return dict(tag=tag, digest=digest, created=created)
-    #         except Exception as ex:
-    #             logging.warning(ex)
-    #             return None
-    #
-    # async def _get_timestamp_for_digests(self, tags_with_digest: List[Dict[str, str]], loop) -> List[Dict[str, str]]:
-    #     """
-    #     loop troough tags with digests and get the corresponding blobs created timestamp
-    #
-    #     :param tags_with_digest:
-    #     :param loop:
-    #     :return:
-    #     """
-    #
-    #     session_blob = aiohttp.ClientSession(
-    #         loop=loop,
-    #         headers=self.http_headers,
-    #         requote_redirect_url=True,
-    #         connector=aiohttp.TCPConnector(
-    #             limit_per_host=50
-    #         )
-    #     )
-    #
-    #     tags_with_digest_and_timestamp = await asyncio.wait([loop.create_task(self._get_timestamp_for_digest(session=session_blob, tag=t['tag'], digest=t['digest'])) for t in tags_with_digest])
-    #     await session_blob.close()
-    #
-    #     results = []
-    #     for r in tags_with_digest_and_timestamp[0]:
-    #         re = r.result()
-    #         results.append(re)
-    #
-    #     return results
-    #
 
 
 class DockerRepositoryQuay(DockerRepositoryV2):
